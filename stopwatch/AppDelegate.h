2a00396eaefe890e2ca1c7ffc627800d62729a8b
Umut Kanbak <uk@kanbak.net>
2013-07-11 17:15:38 -0500

    Initial Commit

---
+++ stopwatch/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  stopwatch
+//
+//  Created by Umut Kanbak on 7/11/13.
+//  Copyright (c) 2013 Umut Kanbak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ stopwatch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  stopwatch
+//
+//  Created by Umut Kanbak on 7/11/13.
+//  Copyright (c) 2013 Umut Kanbak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
